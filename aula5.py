0a51a2f53a8fab598fd6547325fe1dca840a5862
Helen Cavalcanti <helen.cavalcanti@ccc.ufcg.edu.br>
2020-07-02 14:48:50 -0300

    Adicionando aula 5 e aula 6

---
+++ aula5.py
@@ -0,0 +1,65 @@
+#String - texto '' ou ""
+# + *
+#Concatenção
+#       01234 ...
+nome = 'Helen'
+sobrenome = 'Cavalcanti'
+#print(nome + sobrenome)
+
+#Repetição
+#print(nome * 3)
+
+#índice
+#        01234 ...
+#           -1
+nome2 = 'carol'
+i = 4
+#print(nome2[i + 1])
+
+
+#len - tamanho
+#print(len(nome2))
+
+#fatiamento
+
+#print(nome2[:3])
+
+#print(nome.upper())
+#print(nome.lower())
+#print(nome.find("he"))
+
+#in
+#print('x' in nome)
+
+#Comparação
+#print('carol' > 'helen')
+
+#Fortamação
+idade = '18'
+#print('Meu nome é '+ nome +' e minha idade é '+ idade)
+
+print('Meu nome é {} e minha idade é {} '.format(nome, idade))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ aula6.py
@@ -0,0 +1,21 @@
+s = "python rocks"
+#Questão 1
+print(s[3])
+
+#Questão 2
+print(s[2] + s[-5])
+
+#Questão 3
+print(len(s))
+
+#Questão 4
+print(s[len(s)-5])
+
+#Questão 5
+print(s[3:8])
+
+#Questão 6
+print("mel" < "melhor")
+
+#Questão 7
+print("mel" < "Mel")
